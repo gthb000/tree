ecaf4f007c8775baa2bad6fc30aa8c69bae93609
gthb <aliideneme0@gmail.com>
2024-07-05 16:11:17 +0300

    colored lines and max_depth

--- tree.py
+++ tree.py
@@ -7,15 +7,37 @@ class State:
     BRANCH = 2
     LAST   = 3
 
-def tree(file_path, states=[]):
+class Color:
+    DEFAULT = "\033[0m"
+    RED     = "\033[91m"
+    GREEN   = "\033[92m"
+    YELLOW  = "\033[33m"
+    BLUE    = "\033[94m"
+    @classmethod
+    def by_id(cls, _id):
+        return [
+            cls.DEFAULT,
+            cls.RED,
+            cls.GREEN,
+            cls.YELLOW,
+            cls.BLUE,
+        ][_id]
+    @classmethod
+    def paint(cls, color, text):
+        return color+text+cls.DEFAULT
+
+def tree(file_path, states=[], max_depth=0):
     isdir = path.isdir(file_path)
     symbol = "+" if isdir else "-"
-    for state in states:
-        if   state == State.MID:    print("| ", end="")
-        elif state == State.END:    print("  ", end="")
-        elif state == State.BRANCH: print("|-", end="")
-        elif state == State.LAST:   print("'-", end="")
+    for level, state in enumerate(states):
+        indent = "  "
+        if   state == State.MID:    indent = "| "
+        elif state == State.END:    indent = "  "
+        elif state == State.BRANCH: indent = "|-"
+        elif state == State.LAST:   indent = "'-"
+        print(Color.paint(Color.by_id(level%4+1), indent), end="")
     print(symbol, file_path.split("/")[-1])
+    if max_depth != 0 and len(states) >= max_depth: return
     if isdir:
         children = sorted(listdir(file_path))
         child_count = len(children)
@@ -26,15 +48,36 @@ def tree(file_path, states=[]):
                 elif new_states[-1] == State.LAST:   new_states[-1] = State.END
             if   index == child_count-1: new_states.append(State.LAST)
             elif index <  child_count-1: new_states.append(State.BRANCH)
-            tree(file_path+"/"+child, new_states)
+            tree(file_path+"/"+child, new_states, max_depth)
+
+def help():
+    print("""
+Usage:
+    
+    python3 tree.py
+    python3 tree.py <file>
+    python3 tree.py <file> <max_depth>
+""")
 
 def main():
     if len(argv) == 1:
         tree(".")
     elif len(argv) == 2:
-        tree(argv[1].rstrip("/"))
+        if argv[1] == "/":
+            tree("/")
+        else:
+            tree(argv[1].rstrip("/"))
+    elif len(argv) == 3:
+        if argv[2].isdigit():
+            max_depth = int(argv[2])
+        else:
+            help()
+        if argv[1] == "/":
+            tree("/", max_depth=max_depth)
+        else:
+            tree(argv[1].rstrip("/"), max_depth=max_depth)
     else:
-        raise Exception("Invalid Usage: Use with one or two parameters")
+        help()
 
 if __name__ == "__main__":
     main()
