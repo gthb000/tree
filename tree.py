4d830596f329eae1be0a428656e6847baeecf8aa
gthb <aliideneme0@gmail.com>
2024-07-05 15:21:26 +0300

    more clean visualisation

--- tree.py
+++ tree.py
@@ -1,13 +1,26 @@
 from os import listdir, path
 from sys import argv
 
-def tree(file_path, level=0):
+def tree(file_path, states=[]):
     isdir = path.isdir(file_path)
     symbol = "+" if isdir else "-"
-    print(level*"  ", symbol, " ", file_path.split("/")[-1], sep="")
+    for state in states:
+        if   state == "mid":    print("| ", end="")
+        elif state == "end":    print("  ", end="")
+        elif state == "branch": print("|-", end="")
+        elif state == "last":   print("'-", end="")
+    print(symbol, file_path.split("/")[-1])
     if isdir:
-        for child in listdir(file_path):
-            tree(file_path+"/"+child, level+1)
+        children = sorted(listdir(file_path))
+        child_count = len(children)
+        for index, child in enumerate(children):
+            new_states = states.copy()
+            if len(new_states):
+                if   new_states[-1] == "branch": new_states[-1] = "mid"
+                elif new_states[-1] == "last":   new_states[-1] = "end"
+            if   index == child_count-1: new_states.append("last")
+            elif index <  child_count-1: new_states.append("branch")
+            tree(file_path+"/"+child, new_states)
 
 def main():
     if len(argv) == 1:
